0bd1a3d8556924d9abd292653f6f1f23f170049d
Jens Harder <je.harder@gmail.com>
2014-11-07 10:48:24 +0100

    small structure changes

--- Projektgruppe/src/tdti/TDTIGui.java
+++ Projektgruppe/src/tdti/TDTIGui.java
@@ -1,7 +1,5 @@
 package tdti;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -15,10 +13,6 @@ public class TDTIGui extends Gui{
 	private static final long serialVersionUID = 1L;
 	private static TDTIGui gui;
 
-	/**  */
-	private Vertice rootVertice = null;
-	private Vertice currentVertice = null;
-	/** */
 
 	private TDTIGui(){
 		super();
@@ -31,49 +25,19 @@ public class TDTIGui extends Gui{
 		return gui;
 	}
 	
-	@Override
-	public void paintComponent(Graphics g) {
-
-		if(rootVertice != null && autoAlgo && rootVertice instanceof TDTIVertice){
-			((TDTIVertice) rootVertice).algorithmus();
-		}
-
-		super.paintComponent(g);
-
-		//mark current point
-		g.setColor(Color.blue);
-		if(currentVertice != null){
-			g.drawRect(currentVertice.getX(), currentVertice.getY(), currentVertice.getWidth(), currentVertice.getHeight());
-		}
-
-		//draw all vertices recursively
-		if(rootVertice instanceof TDTIVertice){
-			rootVertice.drawTree(g,10,10,780);
-		}
-	}
-
-	public Vertice getRoot(){
-		return rootVertice;
-	}
 
 	@Override
-	protected void calcAlgorithmus() {
+	protected void calcAlgorithmus(boolean repaintBool) {
 		if(rootVertice instanceof TDTIVertice){
 			((TDTIVertice) rootVertice).algorithmus();
 			rootVertice.logSubtree();
 
-			repaint();
+			if(repaintBool){
+				repaint();
+			}
 		} else {
 			System.out.println("No root available");
 		}
-	}
-
-	@Override
-	protected void clearGui() {
-		rootVertice = null;
-		currentVertice = null;
-		//				allVertices = new ArrayList<Vertice>();
-		repaint();
 	}
 
 	@Override
