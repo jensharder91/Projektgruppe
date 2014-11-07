5d43cb450e11be3d83fc865f67fb29bb36552430
Jens Harder <je.harder@gmail.com>
2014-11-07 10:55:15 +0100

    small generic changes. removed all specific code

--- Projektgruppe/src/Gui/Gui.java
+++ Projektgruppe/src/Gui/Gui.java
@@ -3,6 +3,7 @@ package Gui;
 
 import java.awt.BorderLayout;
 import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -10,13 +11,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 
+import Tree.Vertice;
+
 public abstract class Gui extends JPanel{
 
 	protected static final long serialVersionUID = 1L;
+	
+	protected Vertice rootVertice = null;
+	protected Vertice currentVertice = null;
 
 	protected JPanel buttonBar = new JPanel();
 
@@ -53,7 +60,7 @@ public abstract class Gui extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				calcAlgorithmus();
+				calcAlgorithmus(true);
 
 			}
 		});
@@ -64,7 +71,7 @@ public abstract class Gui extends JPanel{
 			public void itemStateChanged(ItemEvent e) {
 				if(e.getStateChange() == ItemEvent.SELECTED){
 					autoAlgo = true;
-					calcAlgorithmus();
+					calcAlgorithmus(true);
 				}else if(e.getStateChange() == ItemEvent.DESELECTED){
 					autoAlgo = false;
 				}
@@ -75,23 +82,44 @@ public abstract class Gui extends JPanel{
 		buttonBar.add(buttonClear);
 		buttonBar.add(toggleAutoAlgo);
 		this.add(buttonBar, "South");
-
-
+	}
+	
+	public Vertice getRoot(){
+		return rootVertice;
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
+		
+		if(rootVertice != null && autoAlgo){
+			calcAlgorithmus(false);
+		}
 
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 		g2.setStroke(new BasicStroke(2));
 		super.paintComponent(g);
+		
+		//mark current point
+		g.setColor(Color.blue);
+		if(currentVertice != null){
+			g.drawRect(currentVertice.getX(), currentVertice.getY(), currentVertice.getWidth(), currentVertice.getHeight());
+		}
+
+		//draw all vertices recursively
+		if(rootVertice != null){
+			rootVertice.drawTree(g,10,10,780);
+		}
 	}
 	
+	protected void clearGui(){
+		rootVertice = null;
+		currentVertice = null;
+		repaint();
+	}
 	
-	protected abstract void calcAlgorithmus();
-	protected abstract void clearGui();
+	protected abstract void calcAlgorithmus(boolean repaintBool);
 	protected abstract void addGuiMouseListener();
 
 }
